b314a98f055b7c154ecdd3f4e9706d7346ee1bd9
Kingsley Ihemelandu <kingsleyihemelanduofficial@gmail.com>
2023-02-15 20:24:42 +0100

    Create signup route

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import EventsPage from "./pages/Events";
 import HomePage from "./pages/Home";
+import SignupPage from "./pages/Signup";
 
 const Router = () => {
   <Routes>
+    <Route path="signup" element={SignupPage} />
     <Route path="/" element={HomePage} />
     <Route path="/events" element={EventsPage} />
   </Routes>;
