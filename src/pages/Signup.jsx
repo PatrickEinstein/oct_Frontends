05592de8ee608390ae1edde0a8eddf883b7dd8f1
Kingsley Ihemelandu <kingsleyihemelanduofficial@gmail.com>
2023-02-16 19:55:42 +0100

    Add inputs and button

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import AuthPage from "../components/AuthPage";
 
 const SignupPage = () => {
@@ -9,6 +10,35 @@ const SignupPage = () => {
         <header>Octave Logo</header>
         <h2>Signup</h2>
         <form action="">
+          <div>
+            <input type="text" placeholder="First Name" />
+          </div>
+
+          <div>
+            <input type="text" placeholder="Last Name" />
+          </div>
+          <div>
+            <input type="text" placeholder="Username" />
+          </div>
+          <div>
+            <input type="password" placeholder="Password" />
+          </div>
+          <div>
+            <input type="password" placeholder="Confirm Password" />
+          </div>
+          <div>
+            <button>Sign Up</button>
+          </div>
+          <div>
+            <div>Login With Facebook</div>
+            <div>Login With Google</div>
+          </div>
+          <div>
+            <p>
+              Already have an account? <Link to={"/login"}>Login</Link>
+            </p>
+          </div>
+          <div></div>
           <button type="submit">Header Text</button>
         </form>
       </section>
