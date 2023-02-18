5cb4a91d92644fcb8df0e8bcb26fa38d6690a69b
Kingsley Ihemelandu <kingsleyihemelanduofficial@gmail.com>
2023-02-18 17:46:04 +0100

    Start redisigning navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "../components/NavBar";
 
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <div className="h-full bg-green-700">
+      <NavBar />I wonder why the home page isn't showing anything
+    </div>
+  );
 };
 
 export default HomePage;
