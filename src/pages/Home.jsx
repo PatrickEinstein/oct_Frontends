c41034a9dbbeed72c1e633990b4fba79918279ad
Kingsley Ihemelandu <kingsleyihemelanduofficial@gmail.com>
2023-02-18 21:42:54 +0100

    Redesign navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import NavBar from "../components/NavBar";
 
 const HomePage = () => {
   return (
-    <div className="h-full bg-green-700">
+    <div className="h-screen">
       <NavBar />I wonder why the home page isn't showing anything
     </div>
   );
