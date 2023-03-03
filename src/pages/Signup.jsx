85a8b332e7464764de87eba2382b852a5ba2b317
Kingsley Ihemelandu <kingsleyihemelanduofficial@gmail.com>
2023-03-03 19:23:15 +0100

    Finish Sign up form layout
    Wait for images to change the image on the left
    Get Facebook and Google Icons
    Wait for confirmation of the design

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import AuthPage from "../components/AuthPage";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import styles from "../styles";
 
 const SignupPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword2, setShowPassword2] = useState(false);
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+
   return (
     <div className="h-screen flex px-3">
-      <section className="flex-1">Image section</section>
-      <section className="flex-1">
+      <section className="flex-1 hidden lg:block">Image section</section>
+      <section className="flex-1 flex flex-col justify-center items-center">
         <header>Octave Logo</header>
-        <h2 className="text-lg">Signup</h2>
-        <form action="" className="border flex flex-col gap-y-3 px-3">
+        <h2 className="text-3xl text-center my-2">Signup</h2>
+        <form
+          action=""
+          className="border rounded-2xl flex flex-col gap-y-5 p-5 max-w-xl w-full"
+        >
           <div className={`${styles.inputContainer}`}>
             <input
               type="text"
               placeholder="First Name"
               className={styles.input}
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
             />
           </div>
 
@@ -24,6 +40,10 @@ const SignupPage = () => {
               type="text"
               placeholder="Last Name"
               className={styles.input}
+              value={lastName}
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
             />
           </div>
 
@@ -32,28 +52,71 @@ const SignupPage = () => {
               type="text"
               placeholder="Username"
               className={styles.input}
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
             />
           </div>
-          <div>
-            <input type="password" placeholder="Password" />
+          <div className={styles.inputContainer}>
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              className={`${styles.input}`}
+              value={password1}
+              onChange={(e) => {
+                setPassword1(e.target.value);
+              }}
+            />
+            <span
+              onClick={() => {
+                setShowPassword(!showPassword);
+              }}
+              className="cursor-pointer h-full flex items-center"
+            >
+              {showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
+            </span>
           </div>
-          <div>
-            <input type="password" placeholder="Confirm Password" />
+
+          <div className={styles.inputContainer}>
+            <input
+              type={showPassword2 ? "text" : "password"}
+              placeholder="Confirm Password"
+              className={`${styles.input}`}
+              value={password2}
+              onChange={(e) => {
+                setPassword2(e.target.value);
+              }}
+            />
+            <span
+              onClick={() => {
+                setShowPassword2(!showPassword2);
+              }}
+              className="cursor-pointer h-full flex items-center"
+            >
+              {showPassword2 ? <AiFillEye /> : <AiFillEyeInvisible />}
+            </span>
           </div>
-          <div>
-            <button>Sign Up</button>
+          <div className={`${styles.buttonContainer} `}>
+            <button
+              type="submit"
+              className={`${styles.button} ${styles.signupButton}`}
+            >
+              Signup
+            </button>
           </div>
-          <div>
+          <div className="flex justify-between">
             <div>Login With Facebook</div>
             <div>Login With Google</div>
           </div>
           <div>
             <p>
-              Already have an account? <Link to={"/login"}>Login</Link>
+              Already have an account?{" "}
+              <Link to={"/login"} className={`text-red-700 font-extrabold`}>
+                Login
+              </Link>
             </p>
           </div>
-          <div></div>
-          <button type="submit">Header Text</button>
         </form>
       </section>
     </div>
--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,11 @@
 const styles = {
-  inputContainer: "bg-red-500 rounded",
-  input: "p-1 bg-transparent",
+  inputContainer: "bg-orange-300 rounded flex items-center px-2",
+  buttonContainer: "flex items-center",
+  input: "p-2 bg-transparent outline-none flex-1 placeholder:text-gray-900",
+  button: "px-2 py-1 block flex-1 rounded text-xl",
+  btnSuccess:
+    "bg-white text-green-700 border border-green-700 hover:text-white hover:bg-green-700 ",
+  signupButton: " bg-red-500 text-white",
 };
 
 export default styles;
