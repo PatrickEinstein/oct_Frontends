8edebaea7b240184f8a87d072e0656b41cb46b44
Kingsley Ihemelandu <kingsleyihemelanduofficial@gmail.com>
2023-03-06 13:43:06 +0100

    Setup redux and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import store from "./app/store";
 import Index from "./pages";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />}></Route>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
